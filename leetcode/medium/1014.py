cb2c95c610049346d9266a65425446dda42c3aa3
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-12-27 22:27:43 +0700

    feat: add best sightseeing pair

---
+++ leetcode/medium/1014.py
@@ -0,0 +1,15 @@
+### https://leetcode.com/problems/best-sightseeing-pair/
+
+from typing import List
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        res = 0
+
+        cur_max = values[0] - 1
+        for i in range(1, len(values)):
+            res = max(res, cur_max + values[i])
+            cur_max = max(cur_max - 1, values[i] - 1)
+
+        return res
