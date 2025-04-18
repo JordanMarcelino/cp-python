7e24578aea9823bee6a3d105101ebee57bc52ee3
Jordan Marcelino <jordan.marcelino@gojek.com>
2025-04-18 23:59:08 +0700

    feat: add count equal and divisible pairs in an array

---
+++ leetcode/easy/2176.py
@@ -0,0 +1,16 @@
+### https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/
+
+from typing import List
+
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+
+        ans = 0
+        for i in range(N):
+            for j in range(i + 1, N):
+                if nums[i] == nums[j] and i * j % k == 0:
+                    ans += 1
+
+        return ans
